3ea93c9a97e21026031c356e2a3c1789a7d33cde
RVleerobertdyer <ldyer@redventures.com>
2026-03-07 22:14:26 -0500

    First todo: added a convo box with enemies to use LLM later

--- src/components/game/combat/Arena.tsx
+++ src/components/game/combat/Arena.tsx
@@ -3,11 +3,18 @@ import { type Combatant as Enemy } from "./types";
 
 interface ArenaProps {
   enemies: Enemy[];
+  background: string;
 }
 
-export default function Arena({ enemies }: ArenaProps) {
+export default function Arena({ enemies, background }: ArenaProps) {
   return (
-    <div className="col-span-4 row-span-4 bg-white rounded-xs w-full flex flex-wrap items-center justify-center gap-4">
+    <div className="col-span-4 row-span-4 bg-white rounded-xs w-full flex flex-wrap items-center justify-center gap-4"
+    style={{
+      backgroundImage: `url(${background})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }}
+    >
       {enemies.map((enemy) => (
         <Combatant key={enemy.id} combatant={enemy} />
       ))}
--- src/components/game/combat/CombatView.tsx
+++ src/components/game/combat/CombatView.tsx
@@ -7,17 +7,21 @@ export default function CombatView() {
     <div className="absolute inset-0 bg-black/75 z-1000 flex flex-col items-center justify-center h-screen">
       <div className="grid grid-cols-6 grid-rows-6 gap-4 w-full h-full">
         <Arena
-          enemies={[
-          skeleton1,
-          skeleton2,
-          skeleton3,
-          ]}
+          background="/backgrounds/dungeon.png"
+          enemies={[skeleton1, skeleton2, skeleton3]}
         />
 
         <PlayerMenu />
 
-        <div className="col-span-4 row-span-2 bg-blue-200 rounded-xs w-full">
-          <h2>MENU</h2>
+        <div className="col-span-4 row-span-2 bg-black/60 rounded-xs w-full border-2 border-yellow-800 p-2 text-white flex flex-col items-center justify-center gap-2">
+          {/* @TODO: LLM Dialogue Box for the enemy! */}
+          <div className="flex flex-col items-center justify-center bg-white rounded-md p-4 text-black w-full">
+            <p>Skeleton: "You seem to be lost! I will not let you pass!"</p>
+          </div>
+          <input type="text" className="bg-white rounded-md p-2 w-full text-black" placeholder="Enter your response..." />
+          <button className="bg-yellow-800/30 text-white rounded-md p-2 w-full cursor-pointer hover:bg-yellow-800/90 transition-all duration-300">
+            Respond
+          </button>
         </div>
       </div>
     </div>
