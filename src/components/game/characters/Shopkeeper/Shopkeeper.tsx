95a8f9209d3f4a0a807836c165f1e0edd48d5f12
leerobertdyer <leerobertdyer@gmail.com>
2026-03-10 23:39:47 -0400

    Huge room refactor to make reusable config keep scene small

--- src/components/game/characters/Shopkeeper/Shopkeeper.tsx
+++ src/components/game/characters/Shopkeeper/Shopkeeper.tsx
@@ -1,22 +1,35 @@
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
-import { ShopkeeperModel } from '../../models/mixamo/Shopkeeper'
+import { ShopkeeperModel } from "../../models/mixamo/Shopkeeper";
+import { Euler, Vector3 } from "three";
 
-export default function Shopkeeper() {
+export default function Shopkeeper({
+  scale,
+  position,
+  rotation
+}: {
+  scale: number;
+  position: Vector3;
+  rotation: [number, number, number];
+}) {
   return (
     <RigidBody
       colliders={false}
       lockRotations
       friction={2}
       mass={10}
-      userData={{ type: "player" }}
-    >
+      type="fixed"
+      scale={scale}
+      position={position}
+      rotation={rotation}
+      >
       <CapsuleCollider
         args={[0.5, 0.4]} // [half-height, radius]
         position={[0, 0.9, 0]}
         restitution={0}
         friction={0}
-      />
+        />
       <ShopkeeperModel />
+        
     </RigidBody>
   );
 }
